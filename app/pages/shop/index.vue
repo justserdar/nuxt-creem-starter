b0a72322f522fea64b5e960b63dc0b69215583f7
h+ <serdar@justserdar.dev>
2025-02-15 16:51:22 +0100

    chore: updated dark mode classes

--- app/pages/shop/index.vue
+++ app/pages/shop/index.vue
@@ -6,14 +6,14 @@
     <template #default>
       <section class="py-6">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <h2 class="font-manrope font-bold text-4xl text-black mb-8 max-lg:text-center">
+          <h2 class="font-manrope font-bold text-4xl text-black dark:text-white mb-8 max-lg:text-center">
             Product list
           </h2>
           <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
             <NuxtLink
               v-for="product in products.items"
               :key="product.id"
-              class="mx-auto sm:mr-0 p-2 group cursor-pointer lg:mx-auto bg-white border-2 border-transparent hover:border-primary transition-all duration-500 rounded-xl"
+              class="mx-auto sm:mr-0 p-2 group cursor-pointer lg:mx-auto bg-white dark:bg-gray-900 border-2 border-transparent hover:border-primary transition-all duration-500 rounded-xl"
               :to="`/shop/products/${product.id}/`"
             >
               <div class="">
@@ -27,7 +27,7 @@
               <div class="mt-5">
                 <div class="flex items-center justify-between">
                   <h6
-                    class="font-semibold text-xl leading-8 text-black transition-all duration-500 group-hover:text-indigo-600"
+                    class="font-semibold text-xl leading-8 text-black dark:text-white transition-all duration-500 group-hover:text-indigo-600"
                   >
                     {{ product.name }}
                   </h6>
